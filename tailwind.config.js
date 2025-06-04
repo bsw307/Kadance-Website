f02fccde7d1bf26032a5b5efb34f060ea5c518c3
Baltasar <baltasar.salamonwelwert@gmail.com>
2025-06-04 05:00:54 -0500

    Updated JSON

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ["./public/**/*.html"],
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1e40af',
+          accent: '#06b6d4',
+        },
+      },
+    },
+    plugins: [],
+  };
+  
+  
